005cf345dc95da726348bf777ef0f73c06639077
TheLivingArrow <thelivingarrow.public@gmail.com>
2026-07-30 15:50:54 +0300

    Added separate zsh configuration, sddm runs on wayland now

---
+++ modules/nixos/zsh.nix
@@ -0,0 +1,15 @@
+{config, lib, nixpkgs}: 
+
+{
+  environment.systemPackages = with pkgs; [
+    zsh
+    zsh-autosuggestions
+    zsh-powerlevel10k
+  ];
+
+  programs.zsh = {
+    enable = true;
+    autosuggestions.enable = true;
+    promptInit ="source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+  };
+}
