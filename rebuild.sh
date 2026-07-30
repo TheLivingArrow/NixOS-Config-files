eae1228391d933d7cd0b7fd58f0cececb373c1ed
TheLivingArrow <thelivingarrow.public@gmail.com>
2026-07-30 16:20:05 +0300

    Better rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
+upgrade=false
+while getopts ":u:" option; do
+  case $option in
+    u)
+      $upgrade=true
+      ;;
+    *)
+      ;;
+  esac
+done
 cd ~/.nixconf
-if sudo nixos-rebuild switch --flake .#laptop; then
+if nixos-rebuild build --flake .#laptop; then
   git commit -a
   git push -u origin main
+  if $upgrade; then
+    sudo nixos-rebuild switch --upgrade --flake .#laptop
+  else
+    sudo nixos-rebuild switch --flake .#laptop
+  fi
 else 
   printf "Something has gone wrong brah, look above.\n"
 fi
