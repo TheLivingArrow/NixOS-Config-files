055e5a548e51ff377fbae847442dea60b77f384c
TheLivingArrow <thelivingarrow.public@gmail.com>
2026-07-30 16:37:36 +0300

    Added message to script yes

--- rebuild.sh
+++ rebuild.sh
@@ -14,6 +14,7 @@ if nixos-rebuild build --no-build-output --flake .#laptop; then
   git commit -a
   git push -u origin main
   if $upgrade; then
+    printf "\nUpgrading system\n"
     sudo nixos-rebuild switch --upgrade --flake .#laptop
   else
     sudo nixos-rebuild switch --flake .#laptop
