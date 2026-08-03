b946877e1507f05c0485c22438458289c7ca8625
TheLivingArrow <thelivingarrow.public@gmail.com>
2026-08-03 19:12:21 +0300

    i hate home manager

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -8,7 +8,6 @@
   # manage.
   home.username = "daniel-nix";
   home.homeDirectory = "/home/daniel-nix";
-  home-manager.useUserPackages = true;  
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
