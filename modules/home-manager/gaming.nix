ae322c080b0ee302c060d81f586cc9564f73ea17
TheLivingArrow <thelivingarrow.public@gmail.com>
2026-08-03 15:32:37 +0300

    gaing.nix

---
+++ modules/home-manager/gaming.nix
@@ -0,0 +1,18 @@
+{
+  home.packages =  [
+    steam
+    lutris
+  ];
+  programs.lutris = {
+    enable = true;
+    extraPackages = with pkgs; [
+      gamemode 
+      gamescope
+      mangohud
+    ];
+  };
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+  };
+}
