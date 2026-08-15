a832f3ba8e45542efbdbb4c4dc701b5948502a30
TheLivingArrow <thelivingarrow.public@gmail.com>
2026-08-15 20:58:18 +0300

    trying to fix app menu for dolphin

--- modules/nixos/apps/file-manager.nix
+++ modules/nixos/apps/file-manager.nix
@@ -1,12 +1,11 @@
 {pkgs, ...}:
 
 {
-  environment= {
-    etc."xdg/menus/applications.menu".source = 
-      "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
-    systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [
       kdePackages.dolphin
       kdePackages.qtsvg
-    ];
-  }
+  ];
+  environment.etc."xdg/menus/applications.menu".source = 
+    "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+
 }
