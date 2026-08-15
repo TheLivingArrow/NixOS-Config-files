822d1b3fefdc1cdbc6f9a7624a48bb127e23a782
TheLivingArrow <thelivingarrow.public@gmail.com>
2026-08-15 23:28:53 +0300

    Maybe now work

--- modules/nixos/apps/file-manager.nix
+++ modules/nixos/apps/file-manager.nix
@@ -4,6 +4,7 @@
   environment.systemPackages = with pkgs; [
       kdePackages.dolphin
       kdePackages.qtsvg
+      kdePackages.plasma-workspace
   ];
   environment.etc."xdg/menus/applications.menu".source = 
     "${pkgs.kdePackages.plasma-workspace}/menu/desktop/plasma-applications.menu";
