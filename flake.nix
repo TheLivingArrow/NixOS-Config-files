776238ed0d1c6640471b94c71ebd4b4fa3c864d7
TheLivingArrow <thelivingarrow.public@gmail.com>
2026-08-16 16:02:13 +0300

    idk

--- flake.nix
+++ flake.nix
@@ -4,11 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     caelestia-shell = {
       url = "github:caelestia-dots/shell";
       inputs.nixpkgs.follows = "nixpkgs";
