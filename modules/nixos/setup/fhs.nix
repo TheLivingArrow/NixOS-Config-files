7748d4c4182159fac33d0721ee1e4f92bd59dee3
TheLivingArrow <thelivingarrow.public@gmail.com>
2026-08-18 16:13:00 +0300

    synyax

--- modules/nixos/setup/fhs.nix
+++ modules/nixos/setup/fhs.nix
@@ -1,6 +1,7 @@
 {config, pkgs, lib}:
 
 {
+  environment.systemPackages = with pkgs; [
     (let base = pkgs.appimageTools.defaultFhsEnvArgs; in
       pkgs.buildFHSEnv (base // {
       name = "fhs";
